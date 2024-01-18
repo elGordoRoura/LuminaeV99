5e4349eda75bae6b41d41931ec04c1e5e7d821e5
Christopher J. Roura <cjroura@gmail.com>
2024-01-18 18:49:18 -0500

    Initial Commit

---
+++ Luminae/LuminaeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuminaeApp.swift
+//  Luminae
+//
+//  Created by Christopher J. Roura on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct LuminaeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
